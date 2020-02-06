5f66714a331c0d4355416f993e701d70d3ffe061
lauraRie <56301211+lauraRie@users.noreply.github.com>
2020-02-06 07:49:46 +0100

    height: 100vh

--- Reactable/src/app/app.component.ts
+++ Reactable/src/app/app.component.ts
@@ -11,9 +11,7 @@ export class AppComponent {
     title = 'Reactable';
 
     ngOnInit() {
-        Playground.CreateScene(
-            
-            );
+        Playground.CreateScene();
     }
 
 }
