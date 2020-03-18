4ec5f0f417775e42166b962497f1515dd3f23c7d
maxomoxo <marco.haider02@gmail.com>
2020-03-18 14:53:26 +0100

    Footer removed

--- Client/src/app/pages/about/about.component.ts
+++ Client/src/app/pages/about/about.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-about',
+  styleUrls: ['./about.component.scss'],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
+  today: number = Date.now();
 }
