d07f15844deedea4d801a63de5c200b06b2569be
grabnerM <56301152+grabnerM@users.noreply.github.com>
2020-03-25 14:44:46 +0100

    Http Service

--- Client/src/app/http.service.ts
+++ Client/src/app/http.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
 
-  constructor(public http : HttpClient) { 
+  constructor(public http: HttpClient) {
   }
-  
+
   public login() {
-    
+
   }
 
   public register() {
