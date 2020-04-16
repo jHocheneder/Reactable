a17764f6af65d350d0c2d355de70446bd33e00fc
grabnerM <56301152+grabnerM@users.noreply.github.com>
2020-04-16 16:03:28 +0200

    Login added constructor

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { NbLoginComponent } from '@nebular/auth';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent extends NbLoginComponent implements OnInit {
+export class LoginComponent implements OnInit {
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
--- Client/src/app/register/register.component.ts
+++ Client/src/app/register/register.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { NbRegisterComponent } from '@nebular/auth';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent extends NbRegisterComponent implements OnInit {
-  
+export class RegisterComponent implements OnInit {
+
+  constructor() {
+
+  }
+
   ngOnInit() {
   }
 
