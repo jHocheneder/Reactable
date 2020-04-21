22c1203cd6a20101157c7c281b57608307cef915
lauraRie <56301211+lauraRie@users.noreply.github.com>
2020-04-21 11:19:41 +0200

    wuerfel klasse erstellt

---
+++ Reactable/src/app/wuerfel.ts
@@ -0,0 +1,12 @@
+export class Wuerfel {
+    public id:Number
+    public x_pos = 0
+    public y_pos = 0
+    public z_pos = 0
+    public teilId:Number
+    public box:any;
+
+    constructor(id:Number) {
+        this.id = id
+    }
+}
