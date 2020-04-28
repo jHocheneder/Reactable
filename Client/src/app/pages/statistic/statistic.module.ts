2a8cf789937274bf9634efef88b1d81c287297d4
maxomoxo <marco.haider02@gmail.com>
2020-04-28 09:57:25 +0200

    Statistic module

--- Client/src/app/pages/statistic/statistic.module.ts
+++ Client/src/app/pages/statistic/statistic.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { NbCardModule } from '@nebular/theme';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { StatisticComponent } from './statistic.component';
 
+import { D3PieComponent } from './d3-pie.component';
+
+const components = [  
+  D3PieComponent
+];
 @NgModule({
   imports: [
     NbCardModule,
-    ThemeModule,
+    ThemeModule,    
+    NgxChartsModule,
+    D3PieComponent
+
   ],
   declarations: [
     StatisticComponent,
+    D3PieComponent
   ],
 })
 export class StatisticModule { }
