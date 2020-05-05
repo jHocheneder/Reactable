4a742a77b315965cb11ed412890a9170497f9cd8
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2020-05-05 11:48:51 +0200

    First Selection

--- Client/src/app/playground.ts
+++ Client/src/app/playground.ts
@@ -1,10 +1,13 @@
 import * as BABYLON from '@babylonjs/core/Legacy/legacy'
 import * as BABYLONMat from '@babylonjs/materials'
 import { Teil } from './datatypes/teil'
+import { HttpService } from './services/http.service'
 
 export class Playground {
   private static engine: BABYLON.Engine
   private static canvas: HTMLCanvasElement
+
+  private http: HttpService;
 
   public static CreateScene() {
     this.canvas = document.getElementById('renderCanvas') as HTMLCanvasElement
@@ -243,6 +246,14 @@ export class Playground {
     let selTeil: Teil = null
     const selMat = new BABYLON.StandardMaterial('selectedMat', scene)
     selMat.diffuseColor = new BABYLON.Color3(1, 1, 1) //white
+
+    let firstselect = true;
+    scene.onPointerObservable.add(function (evt){
+      if(evt.pickInfo.pickedMesh && firstselect){
+        firstselect = false;
+        console.log("First select")
+      }
+    })
 
     //Mesh wählen
     scene.onPointerObservable.add(function (evt) {
