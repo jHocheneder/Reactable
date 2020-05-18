cbc2cf9bfc7274815446d7db534da47390becf8e
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2020-05-18 15:57:09 +0200

    Edit Profile

--- Client/src/app/profile/profile.component.ts
+++ Client/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from '../services/http.service';
+import { sha512 } from 'js-sha512';
 
 @Component({
   selector: 'profile',
@@ -7,13 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { 
-    
+  username: String = "";
+  password: String = "";
+  conpassword: String = "";
+
+  userwarn: String = "";
+  passwordwarn: String = "";
+  conpwdwarn: String = "";
+
+  loggedIn: Boolean = false;
+
+
+  constructor(public router: Router, public http: HttpService) { 
   }
-  
+
+  login() {
+    this.router.navigate(['auth/login']);
+  }
+
+  register(){
+    this.router.navigate(['auth/register']);
+  }
 
   ngOnInit() {
+
+    if(localStorage.getItem('username') != null){
+      this.username = localStorage.getItem('username');
+      this.loggedIn = true;
+    }
     
+  }
+
+  checkUsername(){
+    if(this.username.length<3 || this.username.length>25){
+      this.userwarn = "Der Username muss zwischen 3 und 25 Zeichen lang sein."
+      return true;
+    }
+
+    return false;
+  }
+
+  checkPassword(){
+    if(this.password.length<8){
+      this.passwordwarn = "Das Passwort muss mindestens 8 Zeichen lang sein."
+      return true;
+    }
+    return false;
+  }
+
+  checkConPwd(){
+    if(this.conpassword === this.password){
+      return false;
+    }
+    else{
+      this.conpwdwarn = "Die Passwörter stimmen nicht überein!";
+      return true;
+    }
+  }
+
+  update(){
+    if(!this.checkUsername() && !this.checkPassword() && !this.checkConPwd()){
+      const loginData = { 
+        "username" : this.username,
+        "password": sha512(this.password+"") 
+      };
+      console.log(loginData);
+      //this.http.update(loginData);
+    }
   }
 
 }
