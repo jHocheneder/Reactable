5e37b297509fc0a1eb384a40027be22b9032cf39
maxomoxo <marco.haider02@gmail.com>
2020-06-08 18:46:20 +0200

    stats

--- Client/src/app/pages/statistic/statistic.module.ts
+++ Client/src/app/pages/statistic/statistic.module.ts
@@ -3,11 +3,14 @@ import { NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { StatisticComponent } from './statistic.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
+//import { TrafficChartComponent } from './traffic/traffic-chart.component';
 @NgModule({
   imports: [
     NbCardModule,
     ThemeModule,
+    NgxEchartsModule
   ],
   declarations: [
     StatisticComponent,
