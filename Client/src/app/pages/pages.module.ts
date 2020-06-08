fd2309407b91051a26ff487e475b74f4dd9f9a32
maxomoxo <marco.haider02@gmail.com>
2020-06-08 18:46:04 +0200

    Update pages.module.ts

--- Client/src/app/pages/pages.module.ts
+++ Client/src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { AboutModule } from './about/about.module';
+import { HelpModule } from './help/help.module';
 
 import { StatisticModule } from './statistic/statistic.module';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -17,6 +18,7 @@ import { PagesRoutingModule } from './pages-routing.module';
     DashboardModule,
     StatisticModule,
     AboutModule,
+    HelpModule
   ],
   declarations: [
     PagesComponent,
