5016ad7af5b68b140983263ffe436e3fc13ba165
maxomoxo <marco.haider02@gmail.com>
2020-06-10 14:32:08 +0200

    echarts remove

--- Client/src/app/pages/statistic/statistic.module.ts
+++ Client/src/app/pages/statistic/statistic.module.ts
@@ -3,14 +3,12 @@ import { NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { StatisticComponent } from './statistic.component';
-import { NgxEchartsModule } from 'ngx-echarts';
 import { ChartsModule } from 'ng2-charts';
 //import { TrafficChartComponent } from './traffic/traffic-chart.component';
 @NgModule({
   imports: [
     NbCardModule,
     ThemeModule,
-    NgxEchartsModule,
     ChartsModule
   ],
   declarations: [
