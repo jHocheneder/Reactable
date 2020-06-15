fd2d312d3f40c47d44ced53c2cb926514492f65b
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2020-06-15 16:08:32 +0200

    Removing statistics

--- Client/src/app/pages/statistic/statistic.module.ts
+++ Client/src/app/pages/statistic/statistic.module.ts
@@ -3,13 +3,11 @@ import { NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { StatisticComponent } from './statistic.component';
-import { ChartsModule } from 'ng2-charts';
 //import { TrafficChartComponent } from './traffic/traffic-chart.component';
 @NgModule({
   imports: [
     NbCardModule,
-    ThemeModule,
-    ChartsModule
+    ThemeModule
   ],
   declarations: [
     StatisticComponent,
